919b985e2cb9b4720c5ec73f779539bdc3b43a6f
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-03-10 11:17:48 -0600

    Added new_file, open_file and mad_input functions.

--- mad_libs.py
+++ mad_libs.py
@@ -6,16 +6,30 @@ import pathlib
 import shelve
 
 
-def mad_lib():
-    user_adjective = input("Please enter an adjective: ")
-    user_noun = input("Please enter a noun: ")
-    user_adverb = input("Please enter an adverb: ")
-    user_verb = input("Please enter a verb: ")
-
-    f = open("mad_temp.txt", "r")
-    print(f.name)
-
-    f.close()
+user_name = input("Please name your file: ")
+file_name = user_name + ".txt"
 
 
-mad_lib()
+def new_file():
+    with open("mad_temp.txt", "r") as rf:
+        with open(file_name, "w") as wf:
+            for line in rf:
+                wf.write(line)
+
+
+def open_file():
+    with open(file_name, "r") as f:
+        print(f.name)
+        print(f.read())
+
+
+def mad_input():
+    # input_1 = input("Please enter an adjective: ")
+    # input_2 = input("Please enter a noun: ")
+    # input_3 = input("Please enter an adverb: ")
+    # input_4 = input("Please enter a verb: ")
+    pass
+
+
+new_file()
+open_file()
