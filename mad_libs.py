25b6c10e09db0a9926f5c7108813c9a7d4b3e0f3
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-03-10 21:52:11 -0600

    Removed save input function and wrote repetitve mad_lib function.

--- mad_libs.py
+++ mad_libs.py
@@ -1,5 +1,5 @@
 #! python3
-# mad_libs.py — An exercise in reading and writing to text files.
+"""mad_libs.py — An exercise in reading and writing to text files."""
 
 import re
 import shelve
@@ -10,36 +10,56 @@ file_name = user_name + ".txt"
 
 
 def copy_template():
-    with open("mad_temp.txt", "r") as rf:
-        with open(file_name, "w") as wf:
+    """opens template file and writes it to user-define text file"""
+    with open("mad_temp.txt", "r", encoding="utf-8") as rf:
+        with open(file_name, "w", encoding="utf-8") as wf:
             for line in rf:
                 wf.write(line)
 
 
-def save_input():
-    shelf_file = shelve.open("user_data")
-    user_list = [
-        input("Please enter an adjective: "),
-        input("Please enter a noun: "),
-        input("Please enter a present-tense verb: "),
-        input("Please enter a noun: "),
-    ]
-    shelf_file["user_list"] = user_list
-    print(list(shelf_file.values()))
-    shelf_file.close()
+def mad_lib():
+    """uses regexes to search for NOUN, ADJECTIVE, VERB and ADVERB in file
+    and replaces them with user_data.db values"""
+    with open(file_name, "r", encoding="utf-8") as rf:
+        content = rf.read()
+        with open(file_name, "w", encoding="utf-8") as wf:
+            madlib_regex = re.compile(r"ADJECTIVE")
+            mo_madlib = madlib_regex.sub(
+                input("Please type an adjective: "), content, count=1
+            )
+            # print(mo_madlib)
+            wf.write(mo_madlib)
 
+    with open(file_name, "r", encoding="utf-8") as rf:
+        content = rf.read()
+        with open(file_name, "w", encoding="utf-8") as wf:
+            madlib_regex = re.compile(r"NOUN")
+            mo_madlib = madlib_regex.sub(
+                input("Please type a noun: "), content, count=1
+            )
+            # print(mo_madlib)
+            wf.write(mo_madlib)
 
-def open_file():
-    with open(file_name, "r") as f:
-        content = f.read()
-        noun_regex = re.compile(r"NOUN")
-        mo = noun_regex.search(content)
-        print(mo.group())
+    with open(file_name, "r", encoding="utf-8") as rf:
+        content = rf.read()
+        with open(file_name, "w", encoding="utf-8") as wf:
+            madlib_regex = re.compile(r"VERB")
+            mo_madlib = madlib_regex.sub(
+                input("Please type a past-tense verb: "), content, count=1
+            )
+            # print(mo_madlib)
+            wf.write(mo_madlib)
 
-    # print(f.name)
-    # print(f.read())
+    with open(file_name, "r", encoding="utf-8") as rf:
+        content = rf.read()
+        with open(file_name, "w", encoding="utf-8") as wf:
+            madlib_regex = re.compile(r"NOUN")
+            mo_madlib = madlib_regex.sub(
+                input("Please type a noun: "), content, count=1
+            )
+            # print(mo_madlib)
+            wf.write(mo_madlib)
 
 
 copy_template()
-save_input()
-open_file()
+mad_lib()
